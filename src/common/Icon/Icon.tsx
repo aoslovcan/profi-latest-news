24f2d80cea22822d90eb050f180eaace0d52ecbf
aoslovcan <tonkica1605.o@gmail.com>
2023-03-28 18:43:59 +0200

    add Icon component

---
+++ src/common/Icon/Icon.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {ReactComponent as Search} from "../../assets/icons/Search.svg";
+import { ReactComponent as Home} from "../../assets/icons/Home.svg";
+import {ReactComponent as TVGuide} from "../../assets/icons/TV-Guide.svg";
+import {ReactComponent as News} from "../../assets/icons/News.svg";
+import {ReactComponent as Briefcase} from "../../assets/icons/briefcase.svg";
+import {ReactComponent as Bold} from "../../assets/icons/Bold.svg"
+import {ReactComponent as Health} from "../../assets/icons/Health.svg";
+import {ReactComponent as LabBottle} from "../../assets/icons/lab-bottle-experiment.svg";
+
+type IconProps = {
+    type: string
+}
+
+const Icon = ({type} : IconProps) => {
+
+    const getIcon = () => {
+        switch (type){
+            case "Search":
+                return <Search/>
+            case "Home":
+                return <Home/>
+            case "TVGuide":
+                return <TVGuide/>
+            case "News":
+                return <News/>
+            case "Briefcase":
+                return <Briefcase/>
+            case "Bold":
+                return <Bold/>
+            case "Health":
+                return <Health/>
+            case "LabBottle":
+                return <LabBottle/>
+        }
+    }
+
+    return (
+        <div className="icon">
+            {getIcon()}
+        </div>
+    )
+}
+
+export default Icon
